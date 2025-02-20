ebd23cb1dfb1a5169b16ef54ca1496af6ae7177b
arpitparekh <arpitparekh9@gmail.com>
2025-02-20 10:15:33 +0530

    update

--- rgproject/rgproject/settings.py
+++ rgproject/rgproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,12 +100,7 @@ WSGI_APPLICATION = 'rgproject.wsgi.application'
 
 DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'mydb_rolh',
-            'USER': 'mydb_rolh_user',
-            'PASSWORD': 'aNgGNjRdnAfDOc8fUSoPUoeYzapWsMY0',
-            'HOST': 'dpg-curaftt6l47c73ca6qv0-a',
-            'PORT': '5432',
+            'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
         }
     }
 
@@ -144,6 +140,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+
 
 STATIC_URL = '/static/'
 
