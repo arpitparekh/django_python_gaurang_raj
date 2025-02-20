08390196560d095c085557d4fa8c51dcff1dfe3d
arpitparekh <arpitparekh9@gmail.com>
2025-02-20 09:53:09 +0530

    update

--- rgproject/build.sh
+++ rgproject/build.sh
@@ -2,11 +2,15 @@
 # exit on error
 set -o errexit
 
+# Print current directory and list files for debugging
+echo "Current directory: $(pwd)"
+ls -la
+
 # Install dependencies
 pip install -r requirements.txt
 
-# Install PostgreSQL adapter
-pip install psycopg2-binary dj-database-url
+# Make sure Python can find the modules
+export PYTHONPATH=$(pwd)
 
 # Collect static files
 python manage.py collectstatic --no-input
